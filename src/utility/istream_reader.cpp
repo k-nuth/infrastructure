2d4aac301a0a7c06be7a28a2c42bbfb34d2a1e31
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-06-20 18:32:11 -0300

    modernize-return-braced-init-list

--- src/utility/istream_reader.cpp
+++ src/utility/istream_reader.cpp
@@ -124,7 +124,7 @@ size_t istream_reader::read_size_big_endian()
 code istream_reader::read_error_code()
 {
     const auto value = read_little_endian<uint32_t>();
-    return code(static_cast<error::error_code_t>(value));
+    return {static_cast<error::error_code_t>(value)};
 }
 
 uint16_t istream_reader::read_2_bytes_little_endian()
