48a39e0a3b8f1e0ec5b839e8e3372af1013b8a7f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-01 17:37:53 -0300

    compiler flags

--- conanfile.py
+++ conanfile.py
@@ -43,7 +43,9 @@ class BitprimInfrastructureConan(BitprimConanFile):
                "with_examples": [True, False],
                "microarchitecture": "ANY", #["x86_64", "haswell", "ivybridge", "sandybridge", "bulldozer", ...]
                "fix_march": [True, False],
-               "verbose": [True, False]
+               "verbose": [True, False],
+               "cxxflags": "ANY",
+               "cflags": "ANY",
     }
 
     default_options = "shared=False", \
@@ -55,7 +57,9 @@ class BitprimInfrastructureConan(BitprimConanFile):
         "with_examples=False", \
         "microarchitecture=_DUMMY_",  \
         "fix_march=False", \
-        "verbose=False"
+        "verbose=False", \
+        "cxxflags=_DUMMY_", \
+        "cflags=_DUMMY_"
 
     generators = "cmake"
     exports = "conan_*", "ci_utils/*"
@@ -106,6 +110,8 @@ class BitprimInfrastructureConan(BitprimConanFile):
         self.info.options.with_examples = "ANY"
         self.info.options.verbose = "ANY"
         self.info.options.fix_march = "ANY"
+        self.info.options.cxxflags = "ANY"
+        self.info.options.cflags = "ANY"
 
         #For Bitprim Packages libstdc++ and libstdc++11 are the same
         if self.settings.compiler == "gcc" or self.settings.compiler == "clang":
@@ -136,6 +142,10 @@ class BitprimInfrastructureConan(BitprimConanFile):
         cmake.definitions["WITH_QRENCODE"] = option_on_off(self.options.with_qrencode)
         cmake.definitions["WITH_PNG"] = option_on_off(self.options.with_png)
 
+        if self.options.cxxflags != "_DUMMY_":
+            cmake.definitions["CONAN_CXX_FLAGS"] = cmake.definitions.get("CONAN_CXX_FLAGS", "") + " " + str(self.options.cxxflags)
+        if self.options.cflags != "_DUMMY_":
+            cmake.definitions["CONAN_C_FLAGS"] = cmake.definitions.get("CONAN_C_FLAGS", "") + " " + str(self.options.cflags)
 
         if self.settings.compiler != "Visual Studio":
             # cmake.definitions["CONAN_CXX_FLAGS"] += " -Wno-deprecated-declarations"
