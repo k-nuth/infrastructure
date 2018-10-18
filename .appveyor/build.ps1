02c78b429c47135c996a3177baee4a0bc95a3f41
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-18 11:28:57 -0300

    appveyor workarounds 3

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -1,18 +1,2 @@
-# python build.py
 
-
-If ($env:WAIT_FOR_DEPS -eq 1) {
-    conan user
-    conan remote add bitprim_temp https://api.bintray.com/conan/bitprim/bitprim
-    echo "waiting for secp256k1/0.X@bitprim/staging ..."
-    conan search secp256k1/0.X@bitprim/staging -r bitprim_temp > $null
-    while($env:lastExitCode -eq 1) {
-        Write-Host "."
-        Start-Sleep -s 10
-        conan search secp256k1/0.X@bitprim/staging -r bitprim_temp > $null
-    }
-    Write-Host "secp256k1/0.X@bitprim/staging found"
-}
-Else {
-    python build.py
-}
+python build.py
