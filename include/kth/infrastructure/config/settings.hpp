cb71f03498e5cbce3e20b6af0f7572ed68728562
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-19 19:02:00 -0300

    normalizes code in settings

--- include/kth/infrastructure/config/settings.hpp
+++ include/kth/infrastructure/config/settings.hpp
@@ -1,11 +1,11 @@
 // Copyright (c) 2016-2020 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef KTH_INFRASTUCTURE_CONFIG_SETTINGS_HPP
 #define KTH_INFRASTUCTURE_CONFIG_SETTINGS_HPP
 
-namespace kth {
-namespace config {
+namespace kth::infrastructure::config {
 
 // For configuration settings initialization in other libraries.
 enum class settings
