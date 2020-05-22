8e8a53d16755b24a2652f8e5e0d166ec9121c747
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-22 20:33:00 -0300

    modernizes code in conditional_lock

--- include/kth/infrastructure/utility/conditional_lock.hpp
+++ include/kth/infrastructure/utility/conditional_lock.hpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2016-2020 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef KTH_INFRASTRUCTURE_CONDITIONAL_LOCK_HPP_
 #define KTH_INFRASTRUCTURE_CONDITIONAL_LOCK_HPP_
 
