d922ff9eb3c0a03b4fd7d2614648914c5b979626
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-23 21:08:00 -0300

    improvements in class hd_public

--- include/kth/infrastructure/wallet/hd_public.hpp
+++ include/kth/infrastructure/wallet/hd_public.hpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2016-2020 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef KTH_INFRASTUCTURE_WALLET_HD_PUBLIC_KEY_HPP
 #define KTH_INFRASTUCTURE_WALLET_HD_PUBLIC_KEY_HPP
 
