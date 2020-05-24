eeb1496e03c5f67c58877ff10ba8abc5ccf62070
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-24 19:54:00 -0300

    some improvements to crypto class

--- src/math/crypto.cpp
+++ src/math/crypto.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2016-2020 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <kth/infrastructure/math/crypto.hpp>
 
 #include <kth/infrastructure/math/elliptic_curve.hpp>
