6143672548d4994b313b3b52e743132635f07514
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-25 20:23:00 -0300

    code normalization for cashaddr

--- src/wallet/cashaddr.cpp
+++ src/wallet/cashaddr.cpp
@@ -1,9 +1,10 @@
 // Copyright (c) 2016-2020 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <kth/infrastructure/wallet/cashaddr.hpp>
 
-using data_chunk = bc::data_chunk;
+using data_chunk = kth::data_chunk;
 
 namespace {
 
