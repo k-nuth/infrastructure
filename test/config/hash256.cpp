0a6ec8c96b7b317011345b79e22ec270ba231e77
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-25 21:52:00 -0300

    code modernization for hash256

--- test/config/hash256.cpp
+++ test/config/hash256.cpp
@@ -1,12 +1,13 @@
 // Copyright (c) 2016-2020 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <boost/test/unit_test.hpp>
 #include <boost/program_options.hpp>
 #include <kth/infrastructure.hpp>
 
-using namespace bc;
-using namespace bc::config;
+using namespace kth;
+using namespace kth::infrastructure::config;
 
 BOOST_AUTO_TEST_SUITE(hash256_tests)
 
