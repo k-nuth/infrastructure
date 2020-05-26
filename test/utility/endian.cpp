f36166ec717da2ff15ac480375c2f97de0849edc
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 19:46:00 -0300

    class endian needed to be normalized

--- test/utility/endian.cpp
+++ test/utility/endian.cpp
@@ -1,10 +1,11 @@
 // Copyright (c) 2016-2020 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <boost/test/unit_test.hpp>
 #include <kth/infrastructure.hpp>
 
-using namespace bc;
+using namespace kth;
 
 BOOST_AUTO_TEST_SUITE(endian_tests)
 
