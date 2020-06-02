aa3d046cf34f0d4c1ceae33203e7eb3df3687728
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-02 20:35:00 -0300

    checkpoint uses Catch2

--- test/config/checkpoint.cpp
+++ test/config/checkpoint.cpp
@@ -16,7 +16,7 @@ using namespace kth;
 using namespace kth::infrastructure::config;
 using namespace boost::program_options;
 
-BOOST_AUTO_TEST_SUITE(checkpoint_tests)
+// Start Boost Suite: checkpoint tests
 
 #define CHECKPOINT_HASH_A "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
 #define CHECKPOINT_A CHECKPOINT_HASH_A ":0"
