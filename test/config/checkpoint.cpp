101fb9925e5f52ca9b9b9926ad84b317029ee773
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-02 23:03:00 -0300

    checkpoint uses Catch2

--- test/config/checkpoint.cpp
+++ test/config/checkpoint.cpp
@@ -7,7 +7,7 @@
 
 #include <boost/lexical_cast.hpp>
 #include <boost/program_options.hpp>
-#include <boost/test/unit_test.hpp>
+#include <test_helpers.hpp>
 
 #include <kth/infrastructure.hpp>
 #include <kth/infrastructure/utility/collection.hpp>
