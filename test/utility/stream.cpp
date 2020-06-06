f90d88ed7ae1abdc69461c746100796b05689cb5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-05 23:23:00 -0300

    Remove Boost testing framework in stream

--- test/utility/stream.cpp
+++ test/utility/stream.cpp
@@ -43,16 +43,15 @@ TEST_CASE("stream - roundtrip byte", "[stream tests]") {
     sink.write_byte(expected);
     auto const result = source.read_byte();
 
-    BOOST_REQUIRE(expected == result);
-    BOOST_REQUIRE(stream);
-    BOOST_REQUIRE((bool)sink);
-    BOOST_REQUIRE((bool)source);
-    BOOST_REQUIRE_EQUAL(false, !sink);
-    BOOST_REQUIRE_EQUAL(false, !source);
+    REQUIRE(expected == result);
+    REQUIRE(stream);
+    REQUIRE((bool)sink);
+    REQUIRE((bool)source);
+    REQUIRE(!sink == false);
+    REQUIRE(!source == false);
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_2_bytes_little_endian)
-{
+TEST_CASE("stream - roundtrip 2 bytes little endian", "[stream tests]") {
     const uint16_t expected = 43707;
     std::stringstream stream;
     ostream_writer sink(stream);
