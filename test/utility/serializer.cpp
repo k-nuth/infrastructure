22393b3f2a9c9904c05bcc2a9cc0b07a3c730950
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-05 21:01:00 -0300

    use Catch2 framework in serializer

--- test/utility/serializer.cpp
+++ test/utility/serializer.cpp
@@ -245,15 +245,14 @@ TEST_CASE("serializer - roundtrip variable uint little endian 4 bytes", "[serial
 
     auto const result = source.read_variable_little_endian();
 
-    BOOST_REQUIRE(expected == result);
-    BOOST_REQUIRE((bool)sink);
-    BOOST_REQUIRE((bool)source);
-    BOOST_REQUIRE_EQUAL(false, !sink);
-    BOOST_REQUIRE_EQUAL(false, !source);
+    REQUIRE(expected == result);
+    REQUIRE((bool)sink);
+    REQUIRE((bool)source);
+    REQUIRE(!sink == false);
+    REQUIRE(!source == false);
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_variable_uint_little_endian_8_bytes)
-{
+TEST_CASE("serializer - roundtrip variable uint little endian 8 bytes", "[serializer tests]") {
     uint64_t const expected = 0xd4b14be5d8f02abe;
     data_chunk data(sizeof(uint64_t) + 1);
     auto source = make_safe_deserializer(data.begin(), data.end());
