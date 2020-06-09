ac21cefd07118ad44fa466630e1218035ef7286f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-08 23:39:00 -0300

    use Catch2 in thread

--- test/utility/thread.cpp
+++ test/utility/thread.cpp
@@ -52,39 +52,37 @@ void set_thread_priority_test(int priority) {
 
 // WARNING: This creates a side effect that may impact other tests.
 // We must run these sequentially to prevent concurrency-driven test failures.
-BOOST_AUTO_TEST_CASE(thread__set_thread_priorites__all__set_as_expected)
-{
+TEST_CASE("thread  set thread priorites  all  set as expected", "[thread tests]") {
     // Save so we can restore at the end of this test case.
     int const save = get_thread_priority_test();
 
     set_priority(thread_priority::high);
-    BOOST_REQUIRE_EQUAL(THREAD_PRIORITY_ABOVE_NORMAL, get_thread_priority_test());
+    REQUIRE(THREAD_PRIORITY_ABOVE_NORMAL == get_thread_priority_test());
     set_priority(thread_priority::normal);
-    BOOST_REQUIRE_EQUAL(THREAD_PRIORITY_NORMAL, get_thread_priority_test());
+    REQUIRE(THREAD_PRIORITY_NORMAL == get_thread_priority_test());
     set_priority(thread_priority::low);
-    BOOST_REQUIRE_EQUAL(THREAD_PRIORITY_BELOW_NORMAL, get_thread_priority_test());
+    REQUIRE(THREAD_PRIORITY_BELOW_NORMAL == get_thread_priority_test());
     set_priority(thread_priority::lowest);
-    BOOST_REQUIRE_EQUAL(THREAD_PRIORITY_LOWEST, get_thread_priority_test());
+    REQUIRE(THREAD_PRIORITY_LOWEST == get_thread_priority_test());
 
     // Restore and verify test execution thread priority to minimize side effect.
     set_thread_priority_test(save);
-    BOOST_REQUIRE_EQUAL(save, get_thread_priority_test());
+    REQUIRE(save == get_thread_priority_test());
 }
 
 #else
 
 // WARNING: This creates a side effect that may impact other tests.
 // We must run these sequentially to prevent concurrency-driven test failures.
-BOOST_AUTO_TEST_CASE(thread__set_thread_priorites__all__set_as_expected)
-{
+TEST_CASE("thread  set thread priorites  all  set as expected", "[thread tests]") {
     // Save so we can restore at the end of this test case.
     int const save = get_thread_priority_test();
 
     // Haven't had any luck matching the set and get priority calls as in win.
-    BOOST_REQUIRE_NO_THROW(set_priority(thread_priority::high));
-    BOOST_REQUIRE_NO_THROW(set_priority(thread_priority::normal));
-    BOOST_REQUIRE_NO_THROW(set_priority(thread_priority::low));
-    BOOST_REQUIRE_NO_THROW(set_priority(thread_priority::lowest));
+    REQUIRE_NOTHROW(set_priority(thread_priority::high));
+    REQUIRE_NOTHROW(set_priority(thread_priority::normal));
+    REQUIRE_NOTHROW(set_priority(thread_priority::low));
+    REQUIRE_NOTHROW(set_priority(thread_priority::lowest));
 
     // Restore and verify test execution thread priority to minimize side effect.
     set_thread_priority_test(save);
