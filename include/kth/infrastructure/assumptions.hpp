da2e9dc647d799898196be02fca425a6b485c8ad
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-09-09 13:22:58 -0300

    fix

--- include/kth/infrastructure/assumptions.hpp
+++ include/kth/infrastructure/assumptions.hpp
@@ -8,6 +8,7 @@
 #include <climits>
 #include <cstddef>
 #include <cstdint>
+#include <type_traits>
 
 // This guards assumptions within the codebase.
 static_assert(sizeof(size_t) >= sizeof(uint32_t), "unsupported size_t");
@@ -15,10 +16,10 @@ static_assert(sizeof(size_t) >= sizeof(uint32_t), "unsupported size_t");
 static_assert(sizeof(short) == 2, "16-bit short assumed");      //NOLINT
 static_assert(sizeof(int) == 4, "32-bit int assumed");
 
-// Assumption: We assume 8-bit bytes, because 32-bit int and 16-bit short are assumed.
+// We assume 8-bit bytes, because 32-bit int and 16-bit short are assumed.
 static_assert(CHAR_BIT == 8, "8-bit bytes assumed");
 
-// Assumption: We assume uint8_t is an alias of unsigned char.
+// We assume uint8_t is an alias of unsigned char.
 // char, unsigned char, and std::byte (C++17) are the only "byte types"
 // according to the C++ Standard. "byte type" means a type that can be used to
 // observe an object's value representation. We use uint8_t everywhere to see
@@ -29,8 +30,7 @@ static_assert(CHAR_BIT == 8, "8-bit bytes assumed");
 // http://eel.is/c++draft/cstdint#syn
 static_assert(std::is_same<uint8_t, unsigned char>::value);
 
-// Assumption: We assume two's complement of signed number representation.
+// We assume two's complement of signed number representation.
 static_assert(-1 == ~0, "two's complement signed number representation assumed");
-
 
 #endif // KTH_INFRASTRUCTURE_ASSUMPTIONS_HPP_
