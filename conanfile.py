f202fa8b49a600fa166ba1e9b0d7961bd8046eef
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-01-30 12:36:45 +0100

    bump dependencies

--- conanfile.py
+++ conanfile.py
@@ -58,7 +58,7 @@ class KnuthInfrastructureConan(KnuthConanFileV2):
 
     def build_requirements(self):
         if self.options.tests:
-            self.test_requires("catch2/3.3.2")
+            self.test_requires("catch2/3.5.2")
 
     def requirements(self):
         self.requires("secp256k1/0.18.0", transitive_headers=True, transitive_libs=True)
